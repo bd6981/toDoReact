210307f76f44a3f67796512ba69bbbeaec885508
bd6981 <brittanyjdavis22@gmail.com>
2022-11-08 00:27:27 -0500

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
+import React from 'react'
+import ReactDOM from 'react-dom';
 import './App.css';
 
+
+const { useState } = React;
+
 function App() {
+  const [todoValue, setTodoValue] = useState("");
+  const [todoList, setTodoList] = useState([]);
+  const handlInputChange = event => {
+    setTodoValue(event.target.value);
+  };
+
+  const handleSubmit = () => {
+    setTodoList(prevTodoValue =>
+      prevTodoValue.concat({ value: todoValue })
+    );
+    setTodoValue("");
+  };
+
+  const handleCheckBoxClick = (listnumber, event) => {
+    setTodoList(todoList.map(
+        (item, index) =>
+          {return{
+            ...item,
+            isChecked: index === listnumber ? event.currentTarget.checked : item.isChecked
+          }}
+      
+    ))
+  };
+
   return (
+    // before
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="header">To Do:
+      <input type="text" value={todoValue} onChange={handlInputChange} />
+      <button onClick={handleSubmit}>Save</button>
+      </div>
+      
+      {todoList.map(
+        (item, index) =>
+          item.value && (
+            <li>
+             {/* after  */}
+              <input
+              
+                type="checkbox"
+                id='btn'
+                onClick={handleCheckBoxClick.bind(null, index)}
+                className="strikethrough"
+              />
+              {item.value}
+              {item.isChecked}
+            </li>
+          )
+      )}
     </div>
   );
 }
 
+// const rootElement = document.getElementById("root");
+// ReactDOM.render(<App />, rootElement);
 export default App;
